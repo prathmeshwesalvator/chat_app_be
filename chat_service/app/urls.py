f678229c5bf08a34376de4c699466fd188a116c0
prathmeshwesalvator <prathmesh.wesalvator@gmail.com>
2026-01-26 21:42:23 +0530

    Update URLs to include ChatAPIView for contacts

--- chat_service/app/urls.py
+++ chat_service/app/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from app.views import ChatAPIView
+
 
 from app import views
 
 
 urlpatterns = [
     path("", views.root),
-    path('get-contacts/' , ContactsAPIView.as)
-
+    path('get-contacts/' , ChatAPIView.as_view() , name = 'get-contacts'),
+    path('create-contact/' , ChatAPIView.as_view() , name = 'create-contcat')
 ]
